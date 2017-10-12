a389d316121328106a245df3781e1835c8593ffd
ppreziotti <patpreziotti@gmail.com>
2017-10-11 20:04:20 -0400

    navbar now highlighting current section of the page when scrolling

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,3 +15,19 @@ $("#about-btn").on('click', function() {
 		scrollTop: $('#about').offset().top
 	}, 200);
 });
+
+$(window).on('scroll', function() {
+	var currentPosition = $(window).scrollTop();
+	$('.nav li a').each(function() {
+		var navHeight = $('.navbar-nav').outerHeight() + 1;
+		var sectionLink = $(this);
+		var section = $(sectionLink.attr('href'));
+		if (section.position().top - navHeight <= currentPosition && sectionLink.offset().top + section.height() >= currentPosition) {
+			$('.nav li').removeClass('active');
+			sectionLink.parent().addClass('active');
+		}
+		else {
+			sectionLink.parent().removeClass('active');
+		}
+	});
+});
